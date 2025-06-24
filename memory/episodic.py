6445c3576b4da7e1f23b178c996412a3fa77c600
devs-mohanraj <mohan.ins.nvm@gmail.com>
2025-06-24 23:38:42 +0530

    update "memory"

--- memory/episodic.py
+++ memory/episodic.py
@@ -1,18 +1,17 @@
 import chromadb
-from chromadb.config import Settings
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 
 class EpisodicMemory:
     def __init__(self, persist_dir="data/chroma", collection_name="episodic_memory"):
-        self.client = chromadb.Client(Settings(
-            chroma_db_impl="duckdb+parquet",
-            persist_directory=persist_dir
-        ))
+        self.client = chromadb.PersistentClient(path=persist_dir)
+
         embed_fn = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")  # type: ignore
+
         self.collection = self.client.get_or_create_collection(
             name=collection_name,
             embedding_function=embed_fn
         )
+
         self.count = len(self.collection.get()["ids"])
 
     def store(self, user_input, response):
@@ -26,4 +25,4 @@ class EpisodicMemory:
 
     def search(self, query, k=3):
         results = self.collection.query(query_texts=[query], n_results=k)
-        return [r for r in results["metadatas"][0]]
+        return [r for r in results['metadatas'][0]]
